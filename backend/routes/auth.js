5bdc4c03c9f7e48e016782c1e28bfa7d8f2b513c
Shivanshu <baranwalshivu542@gmail.com>
2023-10-06 20:19:02 +0530

    first commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const User = require('../models/User');
+// const { body, validationResult } = require('express-validator');
+const router = express.Router()
+
+
+//Creating a User using: Post "/api/auth/". Doesn't require Auth
+router.post('/',(req, res) =>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save()
+    res.send(req.body);
+})
+
+module.exports = router;
